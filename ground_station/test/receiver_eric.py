7f6ee74089bc9f8aec135961de74bbc4ee2466e5
RAER2-PC\RAER2 <ebotello9@gmail.com>
2016-09-22 17:08:20 -0400

    edited receiver

--- ground_station/test/receiver_eric.py
+++ ground_station/test/receiver_eric.py
@@ -8,4 +8,10 @@ UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.bind((UDP_IP))
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) #Buffer size 1024
+
+    print("received message:", data)
+    
